d51e425cb707442b77dbe7da6d1c9891fd274afd
wambaugh <marwambaugh@gmail.com>
2022-10-14 14:38:36 -0700

    saving updates to JS to remote

--- assets/script.js
+++ assets/script.js
@@ -52,43 +52,25 @@ function generatePassword() {
   if (numchoice){
     userchoice= userchoice.concat(numbeopt)
   }
-
-}
-
 //userchoice will have all the options ok'd, 
 //take userchoice array & select random char. (RPS) and spec. length. from userlength. 
 //eventuall put them into generatePassword needs to return the final password string. 
 
+//what I wrote
+userchoice = Math.floor(Math.random() * userlength.length);
+generatePassword = userlength[generatePassword];
+return
+}
+
+//from the RPS activity 
+var index = Math.floor(Math.random() * options.length);
+var computerChoice = options[index];
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-
 //acceptance criteria//
 /*GIVEN I need a new, secure password
-
-WHEN I click the button to generate a password
-
-THEN I am presented with a series of prompts for password criteria
-
-WHEN prompted for password criteria
-
-THEN I select which criteria to include in the password
-
-WHEN prompted for the length of the password
-
-THEN I choose a length of at least 8 characters and no more than 128 characters
-
-WHEN asked for character types to include in the password
-
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-
-WHEN I answer each prompt
-
-THEN my input should be validated and at least one character type should be selected
-
-WHEN all prompts are answered
 
 THEN a password is generated that matches the selected criteria
 
